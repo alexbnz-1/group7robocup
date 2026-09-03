0cef38a951b589eec19be42b9ed870bf9c2701f8
alexbnz-1 <127924674+alexbnz-1@users.noreply.github.com>
2026-09-03 16:23:27 +1200

    Added Herkulex library for teensy, verified working on esp32 just need to check on robocup equip

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,78 @@
 #include <Arduino.h>
+#include <HerkulexTeensy.h>
 
-// put function declarations here:
-int myFunction(int, int);
+static const uint8_t SERVO_ID = 0x04;
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+// Teensy 4.0 Serial4:
+// RX = pin 16
+// TX = pin 17
+//
+// This lets you keep the same physical pins
+// you were using on the ESP32.
+HerkulexTeensy herkulex(Serial4, 115200);
+
+void setup()
+{
+    Serial.begin(115200);
+    delay(1500);
+
+    herkulex.begin();
+    herkulex.initializeServo(SERVO_ID);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
+void loop()
+{
+    herkulex.moveAngle(
+        SERVO_ID,
+        -100.0f,
+        250,
+        HerkulexTeensy::LED_BLUE
+    );
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+    delay(1000);
+
+    herkulex.moveAngle(
+        SERVO_ID,
+        -150.0f,
+        500,
+        HerkulexTeensy::LED_GREEN
+    );
+
+    delay(1000);
+
+    herkulex.moveAngle(
+        SERVO_ID,
+        0.0f,
+        1000,
+        HerkulexTeensy::LED_RED
+    );
+
+    delay(1000);
+
+    herkulex.moveAngle(
+        SERVO_ID,
+        100.0f,
+        250,
+        HerkulexTeensy::LED_BLUE
+    );
+
+    delay(1000);
+
+    herkulex.moveAngle(
+        SERVO_ID,
+        150.0f,
+        500,
+        HerkulexTeensy::LED_GREEN
+    );
+
+    delay(1000);
+
+    herkulex.moveAngle(
+        SERVO_ID,
+        0.0f,
+        1000,
+        HerkulexTeensy::LED_RED
+    );
+
+    delay(1000);
 }
