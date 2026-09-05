9d33bf0b3805ae2e62cf5369f76f13db91183a7e
alexbnz-1 <127924674+alexbnz-1@users.noreply.github.com>
2026-09-05 13:54:10 +1200

    Added config files for the herkulex

--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,32 @@
 #include <Arduino.h>
 #include <HerkulexTeensy.h>
 
-static const uint8_t SERVO_ID = 0x04;
+static const uint8_t SERVO_ID = 0x01;
 
-// Teensy 4.0 Serial4:
-// RX = pin 16
-// TX = pin 17
-//
-// This lets you keep the same physical pins
-// you were using on the ESP32.
-HerkulexTeensy herkulex(Serial4, 115200);
+// Teensy 4.0 Serial1:
+// RX = pin 0
+// TX = pin 1
+HerkulexTeensy herkulex(Serial1, 115200);
 
 void setup()
 {
     Serial.begin(115200);
     delay(1500);
 
+    Serial.println("Starting Herkulex library test...");
+
     herkulex.begin();
+
+    Serial.println("Initialising servo...");
     herkulex.initializeServo(SERVO_ID);
+
+    Serial.println("Ready.");
 }
 
 void loop()
 {
+    // -100 degrees in 250 ms
+    Serial.println("-100newcode");
     herkulex.moveAngle(
         SERVO_ID,
         -100.0f,
@@ -31,6 +36,9 @@ void loop()
 
     delay(1000);
 
+
+    // -150 degrees in 500 ms
+    Serial.println("-150");
     herkulex.moveAngle(
         SERVO_ID,
         -150.0f,
@@ -40,6 +48,9 @@ void loop()
 
     delay(1000);
 
+
+    // Centre in 1000 ms
+    Serial.println("0");
     herkulex.moveAngle(
         SERVO_ID,
         0.0f,
@@ -49,6 +60,9 @@ void loop()
 
     delay(1000);
 
+
+    // +100 degrees in 250 ms
+    Serial.println("+100");
     herkulex.moveAngle(
         SERVO_ID,
         100.0f,
@@ -58,6 +72,9 @@ void loop()
 
     delay(1000);
 
+
+    // +150 degrees in 500 ms
+    Serial.println("+150");
     herkulex.moveAngle(
         SERVO_ID,
         150.0f,
@@ -67,6 +84,9 @@ void loop()
 
     delay(1000);
 
+
+    // Centre in 1000 ms
+    Serial.println("0");
     herkulex.moveAngle(
         SERVO_ID,
         0.0f,
